0803eb363d210f4228265329ef72efa9039004f7
arseniy ilyinsky <ilinskiyar@gmail.com>
2022-10-21 21:57:47 +0300

    Add rabbitmq and consumer-queue to docker-compose.yml

--- build-scripts/build-images-simple.sh
+++ build-scripts/build-images-simple.sh
@@ -12,15 +12,15 @@ function build_basic_images() {
 
 function build_jar() {
   # Get count of args
-for var in $@
-  do
-    DIR=$var
-    echo "Building JAR files for ${DIR}"
-    CD_PATH="./${DIR}"
-    cd ${CD_PATH}
-    mvn clean package -T 3 -DskipTests
-    cd ..
-  done
+  for var in $@
+    do
+      DIR=$var
+      echo "Building JAR files for ${DIR}"
+      CD_PATH="./${DIR}"
+      cd ${CD_PATH}
+      mvn clean package -T 3 -DskipTests
+      cd ..
+    done
 }
 
 function build_lib() {
@@ -42,11 +42,11 @@ function pull_or_clone_proj() {
  if cd ${SERVICE_NAME}
   then
  #  git branch -f master origin/master
-   git checkout master
+   git checkout hometask-5
    git pull
    cd ..
   else
-    git clone --branch master ${SERVICE_URL} ${SERVICE_NAME}
+    git clone --branch hometask-5 ${SERVICE_URL} ${SERVICE_NAME}
  fi
 }
 
@@ -54,14 +54,14 @@ function pull_or_clone_proj() {
 cd ..
 
 # Clone or update projects
-pull_or_clone_proj common-module https://github.com/stazhirovka2022/common-module.git
-pull_or_clone_proj medical-monitoring https://github.com/stazhirovka2022/medical-monitoring.git
-pull_or_clone_proj message-analyzer https://github.com/stazhirovka2022/message-analyzer.git
-pull_or_clone_proj person-service https://github.com/stazhirovka2022/person-service.git
+pull_or_clone_proj common-module https://github.com/ooARil/common-module.git
+pull_or_clone_proj medical-monitoring https://github.com/ooARil/medical-monitoring.git
+pull_or_clone_proj message-analyzer https://github.com/ooARil/message-analyzer.git
+pull_or_clone_proj person-service https://github.com/ooARil/person-service.git
+pull_or_clone_proj consumer-queue https://github.com/ooARil/consumer-queue.git
 
 build_lib common-module
-build_jar medical-monitoring message-analyzer person-service
-
+build_jar medical-monitoring message-analyzer person-service consumer-queue
 
 APP_VERSION=0.0.1-SNAPSHOT
 
@@ -69,3 +69,4 @@ echo "Building Docker images"
 build_basic_images ./medical-monitoring/core/target/medical-monitoring-${APP_VERSION}.jar application/medical-monitoring
 build_basic_images ./message-analyzer/core/target/message-analyzer-${APP_VERSION}.jar application/message-analyzer
 build_basic_images ./person-service/core/target/person-service-${APP_VERSION}.jar application/person-service
+build_basic_images ./consumer-queue/core/target/consumer-queue-${APP_VERSION}.jar application/consumer-queue
